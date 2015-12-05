c29977078ab866cf98e578e89fcd9366fe65ceb7
Jefferson Wu <makerjeffwu@gmail.com>
2015-12-04 17:21:49 -0800

    need to restructure the script to modify the global variables

--- myFirstNeoPixel.ino
+++ myFirstNeoPixel.ino
@@ -1,5 +1,8 @@
 /* == First NeoPixel Sketch ==
 Built for Arduino, distilled from C.
+2015.DEC.04:
+  - adding serial input functionality.
+    - set brightness with 1-9.
 */
 
 #include <Adafruit_NeoPixel.h>
@@ -7,13 +10,19 @@ Built for Arduino, distilled from C.
 int PIN = 7;
 int speet = 3;
 
+int red = 100;
+int green = 10;
+int blue = 100;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(9, PIN);
 
 void setup() {
+  
+  Serial.begin(9600);  //fire up a bowl of serial
+  
   strip.begin();
   strip.show();
-  
-  strip.setBrightness(25);
+  strip.setBrightness(100);
 }
 
 void loop() {
@@ -29,8 +38,11 @@ void loop() {
 //  fade(7, 50, 255);
 //  fade(8, 50, 255);
 
-  kittScanner(2);
-  karrScanner(2);
+  //kittScanner(2);
+  //karrScanner(2);
+  
+  customScanner(2);
+  
 
 }
 
@@ -59,9 +71,43 @@ void karrScanner(int repeat) {
     backwardColorWipe(strip.Color(140,255,0), 50);
     backwardColorWipe(strip.Color(0,0,0), 50);
   }
-  
 }
 
+//custom serial input color
+void customScanner(int repeat) {
+  
+    forwardColorWipe(strip.Color(red, green, blue),50);
+    forwardColorWipe(strip.Color(0,0,0),50);
+    
+    backwardColorWipe(strip.Color(red,green,blue),50);
+    backwardColorWipe(strip.Color(0,0,0), 50);
+  
+  while (Serial.available() > 0) {
+    int red = Serial.parseInt();
+    int green = Serial.parseInt();
+    int blue = Serial.parseInt();
+    
+          Serial.print(red);
+          Serial.print(green);
+          Serial.println(blue);
+    
+    //if 'carriage return key' is entered...
+    if(Serial.read() == '\r') {
+//      red = 255-constrain(red, 0, 255);
+//      green = 255-constrain(green, 0, 255);
+//      blue = 255-constrain(green, 0, 255);
+
+      
+
+      
+      forwardColorWipe(strip.Color(red, green, blue),50);
+      forwardColorWipe(strip.Color(0,0,0),50);
+      
+      backwardColorWipe(strip.Color(red,green,blue),50);
+      backwardColorWipe(strip.Color(0,0,0), 50);
+    }
+  }
+}
 
 //UTILITIY FUNCTIONS
 //Color wipe forward
@@ -74,8 +120,8 @@ void forwardColorWipe(uint32_t color, int wait) {
 }
 
 void backwardColorWipe(uint32_t color, int wait) {
-  for(uint16_t i = 9; i >= 0; i--) {
-    strip.setPixelColor(i, color);
+  for(uint16_t i = 9; i > 0; i--) {
+    strip.setPixelColor(i-1, color);
     strip.show();
     delay(wait);
   }
